8044b77dd0c60566522f41d8a20f3af837de76d8
Julian Weinelt <julian.weinelt@outlook.de>
2026-01-06 16:42:25 +0100

    feat: switch themes at runtime

---
+++ src/main/java/de/julianweinelt/databench/ui/ThemeSwitcher.java
@@ -0,0 +1,21 @@
+package de.julianweinelt.databench.ui;
+
+import com.formdev.flatlaf.FlatLaf;
+import javax.swing.*;
+import java.awt.*;
+
+public class ThemeSwitcher {
+
+    public static void switchTheme(FlatLaf newLaf) {
+        try {
+            UIManager.setLookAndFeel(newLaf);
+
+            for (Window window : Window.getWindows()) {
+                SwingUtilities.updateComponentTreeUI(window);
+                window.pack();
+            }
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+    }
+}
