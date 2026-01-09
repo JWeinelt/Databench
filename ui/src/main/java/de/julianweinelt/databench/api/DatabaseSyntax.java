170fb56216a5236f95e30175456df125cf69ecf9
Julian Weinelt <julian.weinelt@outlook.de>
2026-01-09 22:07:33 +0100

    api: add different syntax handling for different database types

---
+++ ui/src/main/java/de/julianweinelt/databench/api/DatabaseSyntax.java
@@ -0,0 +1,70 @@
+package de.julianweinelt.databench.api;
+
+public abstract class DatabaseSyntax {
+    public abstract String showTables();
+    public abstract String showDatabases();
+    public abstract String showViews();
+    public abstract String showFunctions();
+    public abstract String showTriggers();
+
+    public static DatabaseSyntax MSSQL = new DatabaseSyntax() {
+        @Override
+        public String showTables() {
+            return """
+                    SELECT TABLE_NAME
+                    FROM INFORMATION_SCHEMA.TABLES
+                    WHERE TABLE_TYPE = 'BASE TABLE';
+                    """;
+        }
+
+        @Override
+        public String showDatabases() {
+            return "SELECT name, state_desc FROM sys.databases WHERE owner_sid <> 0x01;";
+        }
+
+        @Override
+        public String showViews() {
+            return """
+                    SELECT TABLE_NAME
+                    FROM INFORMATION_SCHEMA.TABLES
+                    WHERE TABLE_TYPE = 'VIEW';
+                    """;
+        }
+
+        @Override
+        public String showFunctions() {
+            return "";
+        }
+
+        @Override
+        public String showTriggers() {
+            return "";
+        }
+    };
+    public static DatabaseSyntax MYSQL = new DatabaseSyntax() {
+        @Override
+        public String showTables() {
+            return "SHOW FULL TABLES IN ${db} WHERE TABLE_TYPE = 'BASE TABLE';";
+        }
+
+        @Override
+        public String showDatabases() {
+            return "SHOW DATABASES;";
+        }
+
+        @Override
+        public String showViews() {
+            return "SHOW FULL TABLES IN ${db} WHERE TABLE_TYPE = 'VIEW';";
+        }
+
+        @Override
+        public String showFunctions() {
+            return "";
+        }
+
+        @Override
+        public String showTriggers() {
+            return "";
+        }
+    };
+}
