cce89c4b6ca61666b356e5dacc03cbd183706537
Julian Weinelt <julian.weinelt@outlook.de>
2026-01-09 22:07:53 +0100

    feat: add MSSQL native windows authentication support

--- ui/src/main/java/de/julianweinelt/databench/api/DatabaseType.java
+++ ui/src/main/java/de/julianweinelt/databench/api/DatabaseType.java
@@ -1,16 +1,23 @@
 package de.julianweinelt.databench.api;
 
 public enum DatabaseType {
-    MYSQL("mysql", "mysql"),
-    MARIADB("mariadb", "mariadb"),
-    MSSQL("sqlserver", "mssql"),
-    POSTGRESQL("postgresql", "postgre");
+    MYSQL("mysql", "mysql", "jdbc:mysql://${server}/${database}" +
+            "?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true", DatabaseSyntax.MYSQL),
+    MARIADB("mariadb", "mariadb", "jdbc:mariadb://${server}/${database}" +
+            "?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true", DatabaseSyntax.MYSQL),
+    MSSQL("sqlserver", "mssql", "jdbc:sqlserver://${server};databaseName=${database};integratedSecurity=true;encrypt=false", DatabaseSyntax.MSSQL),
+    POSTGRESQL("postgresql", "postgre", "jdbc:postgresql://${server}/${database}" +
+            "?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true", DatabaseSyntax.MYSQL);
 
     public final String jdbcString;
     public final String engineName;
+    public final String jdbcURL;
+    public final DatabaseSyntax syntax;
 
-    DatabaseType(String jdbcString, String engineName) {
+    DatabaseType(String jdbcString, String engineName, String jdbcURL, DatabaseSyntax syntax) {
         this.jdbcString = jdbcString;
         this.engineName = engineName;
+        this.jdbcURL = jdbcURL;
+        this.syntax = syntax;
     }
 }
