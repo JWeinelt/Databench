7e43ef56736059d94d1e28d18e601b693abc434e
Julian Weinelt <julian.weinelt@outlook.de>
2026-01-10 20:40:27 +0100

    improvement: load dll libraries via launcher; add file handling

--- launcher/src/main/java/de/julianweinelt/databench/launcher/Launcher.java
+++ launcher/src/main/java/de/julianweinelt/databench/launcher/Launcher.java
@@ -1,5 +1,7 @@
 package de.julianweinelt.databench.launcher;
 
+import de.julianweinelt.databench.launcher.storage.LocalStorage;
+import de.julianweinelt.databench.launcher.ui.FileCopyProgressDialog;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.swing.*;
@@ -11,19 +13,68 @@ import java.util.List;
 
 @Slf4j
 public class Launcher {
-    private final File appFile = new File("../DataBench/DataBench.jar");
+    private final File appFile = new File("DataBench/DataBench.jar");
+    private final File libraryFolder = new File("lib");
+    private final File mssqlLib = new File(libraryFolder, "mssql-jdbc-auth.dll");
+
+    private final LocalStorage storage = new LocalStorage();
 
     public static void main(String[] args) {
         new Launcher().start(args);
-        //TODO: Add JVM args or System.load for dll lib loading for mssql
     }
 
     private void start(String[] args) {
+        // If first start
+        if (!storage.getConfigFile().exists()) {
+            if (libraryFolder.mkdirs()) log.debug("lib folder created successfully");
+            if (appFile.getParentFile().mkdirs()) log.debug("DataBench folder created successfully");
+            try {
+                File tmp1 = new File("DataBench.jar");
+                File tmp2 = new File("mssql-jdbc-auth.dll");
+                Files.copy(tmp1.toPath(), appFile.toPath());
+                if (tmp1.delete()) log.debug("DataBench.jar deleted successfully");
+                Files.copy(tmp2.toPath(), mssqlLib.toPath());
+                if (tmp2.delete()) log.debug("mssql-jdbc-auth.dll deleted successfully");
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+            storage.save();
+        }
+
+        storage.load();
+
         if (args.length != 0) {
             List<String> a = List.of(args);
             if (a.contains("--update")) {
                 update();
+                return;
             }
+        }
+
+        startJar(args);
+    }
+
+    private void startJar(String[] mainArgs) {
+        String[] args = new String[6 + storage.getConfiguration().getJvmArgs().size() + mainArgs.length];
+        args[0] = "java";
+        args[1] = "-Djava.library.path=" + mssqlLib.getAbsolutePath();
+        args[2] = "-Xmx" + storage.getConfiguration().getMaxMemMB() + "M";
+        args[3] = "-Xms" + storage.getConfiguration().getMaxMemMB() + "M";
+        int i = 4;
+        for (String arg : storage.getConfiguration().getJvmArgs()) {
+            args[i++] = arg;
+        }
+        args[i++] = "-jar";
+        args[i++] = appFile.getAbsolutePath();
+        for (String arg : mainArgs) {
+            args[i++] = arg;
+        }
+
+        ProcessBuilder pb = new ProcessBuilder(args);
+        try {
+            pb.start();
+        } catch (IOException e) {
+            log.error(e.getMessage());
         }
     }
 
@@ -37,10 +88,12 @@ public class Launcher {
                     "Update Error", JOptionPane.ERROR_MESSAGE);
             return;
         }
-        try {
-            Files.copy(file.toPath(), appFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+
+        new FileCopyProgressDialog(null, file, appFile, () -> {
+            if (file.delete()) log.debug("Backup deleted successfully");
+            String[] args = new String[1];
+            args[0] = "--update";
+            startJar(args);
+        }).setVisible(true);
     }
 }
