e802b352397d46981676a26dc3ba1312d13e5a0b
Julian Weinelt <julian.weinelt@outlook.de>
2026-01-14 16:07:23 +0100

    api: some changes

--- dbx/src/main/java/de/julianweinelt/databench/dbx/database/ADatabase.java
+++ dbx/src/main/java/de/julianweinelt/databench/dbx/database/ADatabase.java
@@ -1,31 +1,36 @@
 package de.julianweinelt.databench.dbx.database;
 
-import de.julianweinelt.databench.dbx.DbxArchiveWriter;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
+@Getter
 public abstract class ADatabase {
     private final String host;
     private final int port;
     private final String username;
     private final String password;
-    private final DbxArchiveWriter archiveWriter;
 
     public Connection conn;
 
 
-    protected ADatabase(String host, int port, String username, String password, DbxArchiveWriter archiveWriter) {
+    protected ADatabase(String host, int port, String username, String password) {
         this.host = host;
         this.port = port;
         this.username = username;
         this.password = password;
-        this.archiveWriter = archiveWriter;
+    }
+    public static ADatabase of(String host, int port, String username, String password, String db) {
+        return new DBMySQL(host, port, username, password, db);
     }
 
-    public abstract void connect();
+    public abstract boolean connect();
     public void disconnect() {
         try {
             if (conn != null) {
@@ -35,4 +40,17 @@ public abstract class ADatabase {
             log.error(e.getMessage());
         }
     }
+
+    public abstract List<String> getDatabases();
+    public abstract List<String> getTables(String database);
+    public abstract ResultSet getTableData(String database, String table) throws SQLException;
+
+    public abstract SchemaInfo getSchemaInfo(String database);
+
+    public abstract String getDatabaseProductName();
+    public abstract String getDatabaseProductVersion();
+
+
+    public record SchemaInfo(String database, String defaultCharset, String defaultCollation, List<TableInfo> tables) {}
+    public record TableInfo(String name, int rowCount, String engine) {}
 }
