429c38348f4755bb5bec399a4749f2703e0f359a
Julian Weinelt <julian.weinelt@outlook.de>
2026-01-14 09:01:18 +0100

    api: make creation of manifest easy

--- dbx/src/main/java/de/julianweinelt/databench/dbx/model/Manifest.java
+++ dbx/src/main/java/de/julianweinelt/databench/dbx/model/Manifest.java
@@ -1,28 +1,55 @@
 package de.julianweinelt.databench.dbx.model;
 
+import lombok.Getter;
+
+import java.util.Arrays;
 import java.util.List;
 
+@Getter
 public class Manifest {
+    private final String formatVersion = "1.0.0";
+    private long createdAt;
 
-    public String formatVersion = "1.0.0";
-    public String createdAt;
+    private Tool tool;
+    private Source source;
+    private Export export;
 
-    public Tool tool;
-    public Source source;
-    public Export export;
+    public static Manifest create(String version, String dbms, String dbmsVersion, List<String> databases) {
+        Manifest manifest = new Manifest();
+        manifest.createdAt = System.currentTimeMillis();
+        manifest.tool = Tool.create(version);
+        manifest.source = new Source(dbms, dbmsVersion);
+        manifest.export = new Export(databases);
+        return manifest;
+    }
 
+    @Getter
     public static class Tool {
-        public String name;
-        public String version;
+        private final String name;
+        private final String version;
+
+        protected Tool(String name, String version) {
+            this.name = name;
+            this.version = version;
+        }
+
+        public static Tool create(String version) {
+            return new Tool("DataBench", version);
+        }
     }
 
-    public static class Source {
-        public String dbms;
-        public String dbmsVersion;
-    }
+    public record Source(String dbms, String dbmsVersion) {}
 
+    @Getter
     public static class Export {
-        public String mode = "DATABENCH";
-        public List<String> databases;
+        private final String mode = "DBX";
+        private final List<String> databases;
+
+        public Export(List<String> databases) {
+            this.databases = databases;
+        }
+        public Export(String... databases) {
+            this.databases = Arrays.asList(databases);
+        }
     }
 }
