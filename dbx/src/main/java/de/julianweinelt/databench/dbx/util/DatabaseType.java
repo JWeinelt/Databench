a3cb3350f93a0b55aa161a0fcef7fd732225a044
Julian Weinelt <julian.weinelt@outlook.de>
2026-01-14 08:54:07 +0100

    api: remove driverClass from database types as not needed

--- dbx/src/main/java/de/julianweinelt/databench/dbx/util/DatabaseType.java
+++ dbx/src/main/java/de/julianweinelt/databench/dbx/util/DatabaseType.java
@@ -1,3 +1,3 @@
 package de.julianweinelt.databench.dbx.util;
 
-public record DatabaseType(String name, String jdbcLink, String driverClass, String vendor) {}
+public record DatabaseType(String name, String jdbcLink, String vendor) {}
