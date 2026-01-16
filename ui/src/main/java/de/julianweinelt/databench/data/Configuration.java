fc5a815df67784a23a1cbd5c4b1098b08838ba37
Julian Weinelt <julian.weinelt@outlook.de>
2026-01-16 14:41:29 +0100

    improvement: add more configuration options for the appearance of the editor

--- ui/src/main/java/de/julianweinelt/databench/data/Configuration.java
+++ ui/src/main/java/de/julianweinelt/databench/data/Configuration.java
@@ -1,11 +1,16 @@
 package de.julianweinelt.databench.data;
 
 import de.julianweinelt.databench.DataBench;
+import de.julianweinelt.databench.dbx.util.ColorUtil;
 import lombok.Getter;
 import lombok.Setter;
+import org.fife.ui.rsyntaxtextarea.TokenTypes;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -23,6 +28,16 @@ public class Configuration {
     private boolean sendAnonymousData = false;
     private boolean sendErrorProtocols = false;
     private boolean firstStartup = true;
+    private boolean openProjectOnStartup = true;
+    private String closeType = "ask";
+
+    private int editorFontSize = 13;
+    private int generalFontSize = 13;
+    private int projectTreeFontSize = 13;
+
+    private String editorFont = "Consolas";
+    private String editorTheme = "software";
+    private ColorSettings editorColors = new ColorSettings();
 
     private boolean checkForUpdates = true;
     private String updateChannel = "stable";
@@ -31,10 +46,6 @@ public class Configuration {
     public static Configuration getConfiguration() {
         return DataBench.getInstance().getConfigManager().getConfiguration();
     }
-
-    /* --------------------------------------------------
-     * Shortcut API
-     * -------------------------------------------------- */
 
     public KeyStroke getShortcut(String action, KeyStroke defaultKey) {
         String value = shortcuts.get(action);
@@ -58,10 +69,6 @@ public class Configuration {
         return shortcuts.containsKey(action);
     }
 
-    /* --------------------------------------------------
-     * Utility
-     * -------------------------------------------------- */
-
     private String toPersistedString(KeyStroke ks) {
         StringBuilder sb = new StringBuilder();
 
@@ -72,5 +79,82 @@ public class Configuration {
 
         sb.append(java.awt.event.KeyEvent.getKeyText(ks.getKeyCode()));
         return sb.toString().trim();
+    }
+
+    public Font getEditorFontObject() {
+        return new Font(editorFont, Font.PLAIN, editorFontSize);
+    }
+
+
+    public static class ColorSettings {
+        public static final Map<String, Integer> TOKEN_TYPES = Map.ofEntries(
+            Map.entry("NULL", 0),
+            Map.entry("COMMENT_EOL", 1),
+            Map.entry("COMMENT_MULTILINE", 2),
+            Map.entry("COMMENT_DOCUMENTATION", 3),
+            Map.entry("COMMENT_KEYWORD", 4),
+            Map.entry("COMMENT_MARKUP", 5),
+            Map.entry("RESERVED_WORD", 6),
+            Map.entry("RESERVED_WORD_2", 7),
+            Map.entry("FUNCTION", 8),
+            Map.entry("LITERAL_BOOLEAN", 9),
+            Map.entry("LITERAL_NUMBER_DECIMAL_INT", 10),
+            Map.entry("LITERAL_NUMBER_FLOAT", 11),
+            Map.entry("LITERAL_NUMBER_HEXADECIMAL", 12),
+            Map.entry("LITERAL_STRING_DOUBLE_QUOTE", 13),
+            Map.entry("LITERAL_CHAR", 14),
+            Map.entry("LITERAL_BACKQUOTE", 15),
+            Map.entry("DATA_TYPE", 16),
+            Map.entry("VARIABLE", 17),
+            Map.entry("REGEX", 18),
+            Map.entry("ANNOTATION", 19),
+            Map.entry("IDENTIFIER", 20),
+            Map.entry("WHITESPACE", 21),
+            Map.entry("SEPARATOR", 22),
+            Map.entry("OPERATOR", 23),
+            Map.entry("PREPROCESSOR", 24),
+            Map.entry("MARKUP_TAG_DELIMITER", 25),
+            Map.entry("MARKUP_TAG_NAME", 26),
+            Map.entry("MARKUP_TAG_ATTRIBUTE", 27),
+            Map.entry("MARKUP_TAG_ATTRIBUTE_VALUE", 28),
+            Map.entry("MARKUP_COMMENT", 29),
+            Map.entry("MARKUP_DTD", 30),
+            Map.entry("MARKUP_PROCESSING_INSTRUCTION", 31),
+            Map.entry("MARKUP_CDATA_DELIMITER", 32),
+            Map.entry("MARKUP_CDATA", 33),
+            Map.entry("MARKUP_ENTITY_REFERENCE", 34),
+            Map.entry("ERROR_IDENTIFIER", 35),
+            Map.entry("ERROR_NUMBER_FORMAT", 36),
+            Map.entry("ERROR_STRING_DOUBLE", 37),
+            Map.entry("ERROR_CHAR", 38)
+            //Map.entry("DEFAULT_NUM_TOKEN_TYPES", 39)
+        );
+
+        private final HashMap<Integer, String> colors = new HashMap<>();
+
+        public List<String> getAppliedColors() {
+            return new ArrayList<>(colors.values());
+        }
+        public Color getColorForName(String name) {
+            for (Map.Entry<Integer, String> entry : colors.entrySet()) {
+                if (entry.getValue().equals(name)) return ColorUtil.toColor(colors.get(entry.getKey()));
+            }
+            return null;
+        }
+        public int getKey(String name) {
+            return TOKEN_TYPES.getOrDefault(name, -1);
+        }
+
+        public void setColor(int key, Color color) {
+            colors.put(key, ColorUtil.toString(color));
+        }
+        public Color getColor(int key) {
+            String color = colors.getOrDefault(key, null);
+            if (color == null) return null;
+            return ColorUtil.toColor(color);
+        }
+        public void removeColor(int key) {
+            colors.remove(key);
+        }
     }
 }
