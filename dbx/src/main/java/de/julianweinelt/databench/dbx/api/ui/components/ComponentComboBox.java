d76369790e3229b510afe5cf668abfca86203b93
Julian Weinelt <julian.weinelt@outlook.de>
2026-01-16 14:40:24 +0100

    api: add internal names and labels to combo boxes

--- dbx/src/main/java/de/julianweinelt/databench/dbx/api/ui/components/ComponentComboBox.java
+++ dbx/src/main/java/de/julianweinelt/databench/dbx/api/ui/components/ComponentComboBox.java
@@ -5,20 +5,65 @@ import de.julianweinelt.databench.dbx.api.ui.ComponentType;
 
 import javax.swing.*;
 import java.util.HashMap;
+import java.util.function.Consumer;
 
-public class ComponentComboBox extends Component {
+public class ComponentComboBox extends Component<String, ComponentComboBox> {
     private final JComboBox<String> comboBox;
     private final HashMap<String, Runnable> options = new HashMap<>();
+    private final HashMap<String, String> displayLabels = new HashMap<>();
 
     public ComponentComboBox() {
         super(ComponentType.COMBOBOX);
         comboBox = new JComboBox<>();
     }
 
-    public ComponentComboBox option(String name, Runnable action) {
+    public ComponentComboBox option(String name, String label, Runnable action) {
         options.put(name, action);
-        comboBox.addItem(name);
+        comboBox.addItem(label);
+        displayLabels.put(name, label);
         return this;
+    }
+
+    public ComponentComboBox option(String name, String label) {
+        comboBox.addItem(label);
+        displayLabels.put(name, label);
+        return this;
+    }
+
+    @Override
+    public String value() {
+        if (comboBox.getSelectedItem() == null) return "";
+        String val = comboBox.getSelectedItem().toString();
+        for (String s : displayLabels.keySet()) {
+            if (displayLabels.get(s).equals(val)) {
+                return s;
+            }
+        }
+        return "";
+    }
+    public String selectedLabel() {
+        return (comboBox.getSelectedItem() == null) ? "" : comboBox.getSelectedItem().toString();
+    }
+    public int index() {
+        return comboBox.getSelectedIndex();
+    }
+
+    public ComponentComboBox action(Consumer<String> action) {
+        comboBox.addActionListener(i -> action.accept(value()));
+        return this;
+    }
+
+    @Override
+    public ComponentComboBox initialValue(Object val) {
+        if (val instanceof String) {
+            comboBox.setSelectedItem(val);
+        }
+        return this;
+    }
+
+    @Override
+    public boolean expandHorizontally() {
+        return false;
     }
 
     @Override
@@ -27,6 +72,7 @@ public class ComponentComboBox extends Component {
             String selected = (String) comboBox.getSelectedItem();
             if (selected == null) return;
             Runnable action = options.getOrDefault(selected, null);
+            if (action == null) return;
             action.run();
         });
         return comboBox;
