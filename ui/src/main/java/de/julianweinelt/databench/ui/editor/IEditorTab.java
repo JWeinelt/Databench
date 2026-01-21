a6bb50e0bdff96867adca7604d974e102f56e566
Julian Weinelt <julian.weinelt@outlook.de>
2026-01-21 16:38:30 +0100

    fix: move ID for tabs to their respective classes so it can be used to identify tabs

--- ui/src/main/java/de/julianweinelt/databench/ui/editor/IEditorTab.java
+++ ui/src/main/java/de/julianweinelt/databench/ui/editor/IEditorTab.java
@@ -2,12 +2,13 @@ package de.julianweinelt.databench.ui.editor;
 
 import de.julianweinelt.databench.api.DConnection;
 import de.julianweinelt.databench.ui.BenchUI;
+import lombok.Getter;
 
 import javax.swing.*;
 import java.util.UUID;
 
 public interface IEditorTab {
-    UUID id = UUID.randomUUID();
+    UUID getId();
 
     JPanel getTabComponent(BenchUI ui, DConnection connection);
     String getTitle();
