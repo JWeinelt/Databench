9595975cfcd426ee847281297bbad9c4305bee82
Julian Weinelt <julian.weinelt@outlook.de>
2026-02-03 15:35:47 +0100

    feat: add update check to flow; load drivers on startup; load plugins; improve startup logic

--- flow/src/main/java/de/julianweinelt/databench/worker/Flow.java
+++ flow/src/main/java/de/julianweinelt/databench/worker/Flow.java
@@ -1,20 +1,28 @@
 package de.julianweinelt.databench.worker;
 
 import de.julianweinelt.databench.dbx.api.DbxAPI;
+import de.julianweinelt.databench.dbx.api.drivers.DriverManagerService;
 import de.julianweinelt.databench.dbx.api.plugins.PluginLoader;
 import de.julianweinelt.databench.worker.setup.SetupManager;
 import de.julianweinelt.databench.worker.storage.LocalStorage;
 import de.julianweinelt.databench.worker.util.SystemPlugin;
+import de.julianweinelt.databench.worker.util.UpdateChecker;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
 
 @Slf4j
 public class Flow {
+    public static final String version = "1.0.1";
     private static Flow instance;
 
+    @Getter
     private DbxAPI api;
+    @Getter
+    private DriverManagerService driverManagerService;
     @Getter
     private LocalStorage storage;
 
@@ -28,9 +36,20 @@ public class Flow {
     }
 
     private void start() {
-        api = new DbxAPI(new File("api"));
+        api = new DbxAPI(new File("api"), new SystemPlugin());
+        driverManagerService = new DriverManagerService();
+        log.info("Checking for updates...");
+        new UpdateChecker().checkForUpdates(true);
         log.info("Welcome!");
         log.info("Starting DataBench Flow...");
+        log.info("Loading drivers from disk...");
+        try {
+            driverManagerService.preloadDrivers();
+        } catch (IOException e) {
+            log.error("Failed to load drivers from disk (IO): {}", e.getMessage(), e);
+        } catch (SQLException e) {
+            log.error("An internal SQL error occurred loading drivers from disk: {}", e.getMessage(), e);
+        }
         storage = new LocalStorage(new File("config.json"));
         if (!storage.configCreated()) {
             new SetupManager().startCLI();
@@ -39,7 +58,7 @@ public class Flow {
         log.info("Loading local configuration data...");
         storage.load();
         log.info("Loading DBX plugins...");
-        PluginLoader loader = new PluginLoader(api.getRegistry(), new SystemPlugin());
+        PluginLoader loader = new PluginLoader(api);
         loader.loadAll();
     }
 }
