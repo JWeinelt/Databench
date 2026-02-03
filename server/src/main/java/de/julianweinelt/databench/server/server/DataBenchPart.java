f94f2a8d4474de98b4929dce57bbe0d1d2b5ea23
Julian Weinelt <julian.weinelt@outlook.de>
2026-02-03 15:36:13 +0100

    refactor: rename server entry of "worker" to "flow"

--- server/src/main/java/de/julianweinelt/databench/server/server/DataBenchPart.java
+++ server/src/main/java/de/julianweinelt/databench/server/server/DataBenchPart.java
@@ -3,7 +3,7 @@ package de.julianweinelt.databench.server.server;
 public enum DataBenchPart {
     EDITOR("ui"),
     LAUNCHER("launcher"),
-    WORKER("worker");
+    FLOW("flow");
 
     public final String folder;
 
