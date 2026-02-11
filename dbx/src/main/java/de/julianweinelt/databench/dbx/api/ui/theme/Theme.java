c59d847976175fd006efa04439a6249a97daee34
Julian Weinelt <julian.weinelt@outlook.de>
2026-02-11 16:04:10 +0100

    api: make plugins able to add new themes (either using java class or using .theme.json like in IntelliJ)

--- dbx/src/main/java/de/julianweinelt/databench/dbx/api/ui/theme/Theme.java
+++ dbx/src/main/java/de/julianweinelt/databench/dbx/api/ui/theme/Theme.java
@@ -1,16 +1,28 @@
 package de.julianweinelt.databench.dbx.api.ui.theme;
 
 import de.julianweinelt.databench.dbx.api.plugins.DbxPlugin;
+import lombok.Getter;
 
+import javax.swing.plaf.basic.BasicLookAndFeel;
+
+@Getter
 public class Theme {
     private final DbxPlugin definingPlugin;
     private final String unlocalizedName;
 
     private final String themeData;
+    private BasicLookAndFeel lafClass = null;
 
     public Theme(DbxPlugin definingPlugin, String unlocalizedName, String themeData) {
         this.definingPlugin = definingPlugin;
         this.unlocalizedName = unlocalizedName;
         this.themeData = themeData;
+    }
+
+    public Theme(DbxPlugin definingPlugin, String unlocalizedName, String themeData, BasicLookAndFeel lafClass) {
+        this.definingPlugin = definingPlugin;
+        this.unlocalizedName = unlocalizedName;
+        this.themeData = themeData;
+        this.lafClass = lafClass;
     }
 }
