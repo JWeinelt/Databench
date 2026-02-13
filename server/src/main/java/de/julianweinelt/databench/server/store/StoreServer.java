fd721825b9d2e2cadd1761df6076ca567436de35
Julian Weinelt <julian.weinelt@outlook.de>
2026-02-13 15:11:54 +0100

    improvement: improve icons and branding

--- server/src/main/java/de/julianweinelt/databench/server/store/StoreServer.java
+++ server/src/main/java/de/julianweinelt/databench/server/store/StoreServer.java
@@ -6,9 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Objects;
 
 @Slf4j
 public class StoreServer {
+    private boolean maintenance = true;
     private Javalin app;
 
     public void start() {
@@ -19,7 +21,24 @@ public class StoreServer {
             ctx.html(Files.readString(new File("public-store/error/404.html").toPath()));
         }).error(503, ctx -> {
             ctx.html(Files.readString(new File("public-store/error/503.html").toPath()));
-        })
+        }).before(ctx -> {
+                    if (!maintenance) {
+                        return;
+                    }
+
+                    String path = ctx.path();
+
+                    boolean isStaticFile = path.contains(".");
+
+                    if (isStaticFile) {
+                        return;
+                    }
+
+                    ctx.status(503);
+                    ctx.contentType("text/html");
+                    ctx.result(Files.readString(new File("public-store/maintenance/index.html").toPath()));
+                    ctx.skipRemainingHandlers();
+                })
         .start(7001);
     }
 }
